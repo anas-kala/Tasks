f491064f43872e88a55f5a7716b4b8997a5f6c2b
Anas Al Kala <aal1580@students.fhv.at>
2021-10-17 13:42:02 +0200

    Add project files.

---
+++ Tasks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskClass tc = new TaskClass();
+            Console.WriteLine("without waiting all the tasks to finish");
+            foreach(var str in tc.GetStringList())
+            {
+                Console.WriteLine(str.Result);
+            }
+
+            Console.WriteLine("\nwaiting all the tasks to finish");
+            foreach (var str in tc.GetStringListWAITALL())
+            {
+                Console.WriteLine(str.Result);
+            }
+        }
+    }
+}
---
+++ Tasks/TaskClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class TaskClass
+    {
+        public List<Task<string>> GetStringListWAITALL()
+        {
+            List<Task<string>> list = new List<Task<string>>();
+            for (int i = 0; i < 10; i++)
+            {
+                Task<string> t = Task.Run(() => GenerateRandomString());
+                list.Add(t);
+            }
+            Task.WaitAll(list.ToArray());
+            var list1 = Task.WhenAll(list);
+            return list;
+        }
+
+        public List<Task<string>> GetStringList()
+        {
+            List<Task<string>> list = new List<Task<string>>();
+            for (int i = 0; i < 10; i++)
+            {
+                var t = Task.Run(() => GenerateRandomString());
+                list.Add(t);
+            }
+            return list;
+        }
+
+        public string GenerateRandomString()
+        {
+            Random random = new Random();
+            int length = 16;
+            var rString = "";
+            for (var i = 0; i < length; i++)
+            {
+                rString += ((char)(random.Next(1, 26) + 64)).ToString().ToLower();
+            }
+            Thread.Sleep(1000);
+            return rString;
+        }
+    }
+}
